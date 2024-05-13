48234a2bf60bf0f25aefe857a97cbef4237f9a43
zeeshan-rajpot <zeeshanrajpot74@gmail.com>
2024-05-13 17:55:47 +0500

    ui completed

--- src/Pages/TakeTest/index.jsx
+++ src/Pages/TakeTest/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Stepper from "../../Compunents/Stepper";
 import Navbar from "../../Compunents/Navbar";
+import Footer from "../../Compunents/Footer";
 
 const index = () => {
   return (
@@ -11,37 +12,8 @@ const index = () => {
         <p className="text-sm text-[#1A9AFF] p-3 border">00:02:39</p>
       </div>
       <Stepper />
-      <div className=" py-0 md:py-12 flex justify-center ">
-        <p className="text-xl lg:text-3xl">Choose the missing figure</p>
-      </div>
-      <div className="flex justify-center">
-        <img src="pngegg 1.png" alt="" className="w-9/12  lg:w-4/12" />
-      </div>
-      <div className="grid grid-cols-2 md:grid-cols-3 place-items-center">
-        <div>
-          <img src="Frame 39.png" alt="" className="w-4/12" />
-        </div>
-        <div>
-          {" "}
-          <img src="Frame 40.png" alt="" className="w-4/12" />
-        </div>
-        <div>
-          {" "}
-          <img src="Frame 41.png" alt="" className="w-4/12" />
-        </div>
-        <div>
-          {" "}
-          <img src="Frame 39 (1).png" alt="" className="w-4/12" />
-        </div>
-        <div>
-          {" "}
-          <img src="Frame 40 (1).png" alt="" className="w-4/12" />
-        </div>
-        <div>
-          {" "}
-          <img src="Frame 41 (1).png" alt="" className="w-4/12" />
-        </div>
-      </div>
+      <Footer/>
+    
     </>
   );
 };
